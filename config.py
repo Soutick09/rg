c12107b5f4852114b7253d95c64593b2fb9834b2
Soutick <soutickadak99@gmail.com>
2024-12-22 09:50:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,81 +1,69 @@
 import os
-
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 
 def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
+    if isinstance(value, str):
+        if value.lower() in ["true", "yes", "1", "enable", "y"]:
+            return True
+        elif value.lower() in ["false", "no", "0", "disable", "n"]:
+            return False
+    return default
 
 
 # Mandatory variables for the bot to start
 # API ID from https://my.telegram.org/auth
-API_ID = int(os.environ.get("0"))
-# API Hash from https://my.telegram.org/auth
-API_HASH = os.environ.get("9fa30733d7a99b212485393e6b5c5ceb")
-BOT_TOKEN = os.environ.get("7065180942:AAEqo9Tn0PMpdLjBfk-wYO2DNZ80gygsXMA")  # Bot token from @BotFather
+API_ID = int(os.environ.get("API_ID", 0))  # Default to 0 if not set
+API_HASH = os.environ.get("API_HASH", "")  # Empty string as default
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Empty string as default
+
 ADMINS = (
-    [int(i.strip()) for i in os.environ.get("5827289728").split(",")]
-    if os.environ.get("5827289728")
+    [int(i.strip()) for i in os.environ.get("ADMINS", "").split(",")]
+    if os.environ.get("ADMINS")
     else []
 )
 
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Shortnerbot")
-DATABASE_URL = os.environ.get(
-    "mongodb+srv://shortnerbot:GkeGvUjcRGOunkoi@shortnerbot.cfwbjti.mongodb.net/?retryWrites=true&w=majority&appName=Shortnerbot", None
-)  # mongodb uri from https://www.mongodb.com/
-OWNER_ID = int(os.environ.get("7272399911"))  # id of the owner
-ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+DATABASE_URL = os.environ.get("DATABASE_URL", "")  # Default to empty if not set
+OWNER_ID = int(os.environ.get("OWNER_ID", 0))  # Default to 0 if not set
+if OWNER_ID and OWNER_ID not in ADMINS:
+    ADMINS.append(OWNER_ID)
 
-#  Optionnal variables
-LOG_CHANNEL = int(
-    os.environ.get("LOG_CHANNEL", "-1002434243644")
-)  # log channel for information about users
-UPDATE_CHANNEL = int(os.environ.get(
-    "UPDATE_CHANNEL", False))  # For Force Subscription
-BROADCAST_AS_COPY = is_enabled(
-    (os.environ.get("BROADCAST_AS_COPY", "False")), False
-)  # true if forward should be avoided
-IS_PRIVATE = is_enabled(
-    os.environ.get("IS_PRIVATE", "False"), "False"
-)  # true for private use and restricting users
-SOURCE_CODE = os.environ.get(
-    "SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2"
-)  # for upstream repo
-# image when someone hit /start
+# Optional variables
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))  # Default to 0
+UPDATE_CHANNEL = int(os.environ.get("UPDATE_CHANNEL", "0"))  # Default to 0
+BROADCAST_AS_COPY = is_enabled(os.environ.get("BROADCAST_AS_COPY", "False"), False)
+IS_PRIVATE = is_enabled(os.environ.get("IS_PRIVATE", "False"), False)
+SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/kevinnadar22/URL-Shortener-V2")
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", "")
-LINK_BYPASS = is_enabled(
-    (os.environ.get("LINK_BYPASS", "False")), False
-)  # if true, urls will be bypassed
-# your shortener site domain
+LINK_BYPASS = is_enabled(os.environ.get("LINK_BYPASS", "False"), False)
 BASE_SITE = os.environ.get("BASE_SITE", "droplink.co")
 
 # For Admin use
-CHANNELS = is_enabled((os.environ.get("CHANNELS", "True")), True)
+CHANNELS = is_enabled(os.environ.get("CHANNELS", "True"), True)
 CHANNEL_ID = (
-    [int(i.strip()) for i in os.environ.get("CHANNEL_ID").split(" ")]
+    [int(i.strip()) for i in os.environ.get("CHANNEL_ID", "").split(",")]
     if os.environ.get("CHANNEL_ID")
     else []
 )
 
 DE_BYPASS = (
-    [i.strip() for i in os.environ.get("DE_BYPASS").split(",")]
+    [i.strip() for i in os.environ.get("DE_BYPASS", "").split(",")]
     if os.environ.get("DE_BYPASS")
     else []
 )
 DE_BYPASS.append("mdisk.me")
 
-FORWARD_MESSAGE = is_enabled(
-    (os.environ.get("FORWARD_MESSAGE", "False")), False
-)  # true if forwardd message to converted by reposting the post
+FORWARD_MESSAGE = is_enabled(os.environ.get("FORWARD_MESSAGE", "False"), False)
 
-
+# Web server settings
 WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "True"), True)
-PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))
-PORT = int(os.environ.get("PORT", "8000"))
+PING_INTERVAL = int(os.environ.get("PING_INTERVAL", "240"))  # Default to 240 seconds
+PORT = int(os.environ.get("PORT", "8000"))  # Default to port 8000
+
+# Validate mandatory variables
+if not API_ID or not API_HASH or not BOT_TOKEN:
+    raise ValueError("Missing mandatory environment variables: API_ID, API_HASH, BOT_TOKEN.")
